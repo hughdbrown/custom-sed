644f72db36514802e5a67c6170e5744f05155add
Hugh Brown <hughdbrown@yahoo.com>
2019-09-09 18:30:13 -0400

    Indent In try-block; use delete_lines

--- src/apps/absolute_import.py
+++ src/apps/absolute_import.py
@@ -16,7 +16,8 @@ class AbsoluteImportInjector(SedFile):
 
     def modify(self):
         self._modify_absolute()
-        self._modify_multiple()
+        # self._modify_multiple()
+        self._modify_pylint()
 
     def _modify_absolute(self):
         """
@@ -25,42 +26,45 @@ class AbsoluteImportInjector(SedFile):
         """
         if any(self.data) and not any("absolute_import" in line for line in self.data):
             print(self.filename)
-            i = 0
-            if self.data[i].startswith(("#!/")):
-                # Skip execution header
-                i += 1
-
-            if self.data[i].startswith("#") and "coding" in self.data[i]:
-                # Skip file encoding comment
-                i += 1
-
-            comment_header = ("'''", '"""')
-            if self.data[i].lstrip().startswith(comment_header):
-                # Skip any docstring
-                if len(self.data[i]) == 3 or not self.data[i].endswith(comment_header):
-                    # If the comment does not start and end on this line ...
+            try:
+                i = 0
+                if self.data[i].startswith(("#!/")):
+                    # Skip execution header
                     i += 1
-                    try:
-                        while not self.data[i].endswith(comment_header):
-                            # print(i, self.data[i])
-                            i += 1
-                    except IndexError:
-                        return None
-                assert self.data[i].endswith(comment_header)
-                i += 1
 
-            for j, line in enumerate(self.data, start=i):
-                # Skip other comments
-                if not line.lstrip().startswith("#"):
-                    break
-                i = j
+                if self.data[i].startswith("#") and "coding" in self.data[i]:
+                    # Skip file encoding comment
+                    i += 1
 
-            # This is the first line that a future import might be placed
-            if self.data[i].startswith(self.prefix):
-                self.data[i] += ", absolute_import"
-            else:
-                self.data = self.data[:i] + [self.injection] + self.data[i:]
-            self.modified = True
+                comment_header = ("'''", '"""')
+                if self.data[i].lstrip().startswith(comment_header):
+                    # Skip any docstring
+                    if len(self.data[i]) == 3 or not self.data[i].endswith(comment_header):
+                        # If the comment does not start and end on this line ...
+                        i += 1
+                        try:
+                            while not self.data[i].endswith(comment_header):
+                                # print(i, self.data[i])
+                                i += 1
+                        except IndexError:
+                            return None
+                    assert self.data[i].endswith(comment_header)
+                    i += 1
+
+                for j, line in enumerate(self.data, start=i):
+                    # Skip other comments
+                    if not line.lstrip().startswith("#"):
+                        break
+                    i = j
+
+                # This is the first line that a future import might be placed
+                if self.data[i].startswith(self.prefix):
+                    self.data[i] += ", absolute_import"
+                else:
+                    self.data = self.data[:i] + [self.injection] + self.data[i:]
+                self.modified = True
+            except IndexError:
+                pass
 
     def _modify_multiple(self):
         """Flatten multiple (possibly non-conecutive) matches into a single line"""
@@ -76,6 +80,10 @@ class AbsoluteImportInjector(SedFile):
                 line for j, line in enumerate(self.data) if j not in set(x[1:])
             ]
             self.modified = True
+
+    def _modify_pylint(self):
+        j = {i for i, line in enumerate(self.data) if line.startswith('# pylint: disable=no-absolute-import')}
+        self.delete_lines(j)
 
 
 if __name__ == "__main__":
