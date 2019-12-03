7aa06b5ba087fe459655063e819a9514928a2c98
Hugh Brown <hughdbrown@yahoo.com>
2019-12-03 17:20:18 -0500

    Add src/apps/documented_as_of.py

---
+++ src/apps/documented_as_of.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+from __future__ import absolute_import
+
+import re
+
+from lib import SedFile, main
+
+RE_NEXT = re.compile(
+    r"""
+    ^
+    (?P<left>\s*.*?)
+    public_as_of='
+    (?P<public>[a-zA-Z0-9_\.]+)
+    '
+    (?P<middle>.*?)
+    documented_as_of='
+    (?P<documented>[a-zA-Z0-9_\.]+)
+    '
+    (?P<right>.*)
+    $
+""",
+    re.VERBOSE,
+)
+
+
+class PublicDocumented(SedFile):
+    """
+    Derivative of SedFile dedicated to removing redundant 'documented_as_of'
+    """
+
+    def modify(self):
+        match_iter = ((i, RE_NEXT.match(line)) for i, line in enumerate(self.data))
+        matches = [(i, m) for i, m in match_iter if m]
+        for i, m in matches:
+            g = m.groupdict()
+            left, middle, right = g["left"], g["middle"], g["right"]
+            public, documented = g["public"], g["documented"]
+            if public == documented:
+                if middle == ', ' and right == ')':
+                    self.data[i] = "{}public_as_of='{}')".format(left, public)
+                else:
+                    self.data[i] = "{}public_as_of='{}'{}{}".format(
+                        left, public, middle, right
+                    )
+                self.modified = True
+
+
+if __name__ == "__main__":
+    main(PublicDocumented)
